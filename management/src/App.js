c063dadf9b948fb4029c2da0dbd4e57b27909a79
sucu <hasm0326@naver.com>
2023-02-21 15:25:55 +0900

    Create React Management

---
+++ management/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="gray-background">
+        <img src={logo} alt="logo" />
+        <h2>develop management system!!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
